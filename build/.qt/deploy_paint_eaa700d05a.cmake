e02e85c7f415e7ba09c0f87d2a35402ded44ba5d
Woratt <162185253+Woratt@users.noreply.github.com>
2025-09-28 10:36:23 +0200

    Delete build directory

--- build/.qt/deploy_paint_eaa700d05a.cmake
+++
@@ -1,7 +0,0 @@
-include("/Users/admin/Downloads/education/Paint/build/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/paint-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "paint.app"
-)
--- build/tests/CMakeFiles/test_paint.dir/test_paint_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-tests/CMakeFiles/test_paint.dir/test_paint_autogen/mocs_compilation.cpp.o: \
-  /Users/admin/Downloads/education/Paint/build/tests/test_paint_autogen/mocs_compilation.cpp
